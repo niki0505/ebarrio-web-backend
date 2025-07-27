b55cbcf87200d032cf21312252b25ce8e4c27a77
niki0505 <bataclanknicole2004@gmail.com>
2025-07-27 20:10:01 +0800

    Get chats

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,5 +1,62 @@
 import FAQ from "../models/FAQs.js";
 import ActivityLog from "../models/ActivityLogs.js";
+import Chat from "../models/Chats.js";
+
+export const getChats = async (req, res) => {
+  try {
+    const chats = await Chat.find()
+      .populate({
+        path: "participants",
+        select: "resID empID",
+        populate: [
+          { path: "resID", select: "firstname lastname picture" },
+          {
+            path: "empID",
+            populate: {
+              path: "resID",
+              select: "firstname lastname picture",
+            },
+          },
+          ,
+        ],
+      })
+      .populate({
+        path: "messages.from",
+        select: "resID empID",
+        populate: [
+          { path: "resID", select: "firstname lastname picture" },
+          {
+            path: "empID",
+            populate: {
+              path: "resID",
+              select: "firstname lastname picture",
+            },
+          },
+          ,
+        ],
+      })
+      .populate({
+        path: "messages.to",
+        select: "resID empID",
+        populate: [
+          { path: "resID", select: "firstname lastname picture" },
+          {
+            path: "empID",
+            populate: {
+              path: "resID",
+              select: "firstname lastname picture",
+            },
+          },
+          ,
+        ],
+      });
+
+    return res.status(200).json(chats);
+  } catch (error) {
+    console.error("Error in getting chats:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const getFAQs = async (req, res) => {
   try {
