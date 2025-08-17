68e647f894a0a49f28340f7673a436c3e2d2837e
niki0505 <bataclanknicole2004@gmail.com>
2025-08-17 18:02:31 +0800

    Get SOS added

--- routes/routes.js
+++ routes/routes.js
@@ -151,6 +151,7 @@ import {
   getFAQs,
 } from "../controllers/chatController.js";
 import { analyticsAI, getPrompts } from "../controllers/analyticsController.js";
+import { getReports } from "../controllers/SOSController.js";
 
 const router = express.Router();
 
@@ -393,4 +394,7 @@ router.put("/endchat/:chatID", authMiddleware, endChat);
 //ANALYTICS
 router.post("/analytics", authMiddleware, analyticsAI);
 router.get("/getprompts", authMiddleware, getPrompts);
+
+//SOS
+router.get("/getreports", authMiddleware, getReports);
 export default router;
